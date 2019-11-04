a2910702d1dbed69ebf4532440e3482aca56278e
MinysJava <poforli@gmail.com>
2019-11-04 16:24:57 +0500

    task_903

--- src/main/java/onehundred_ohTen/task_903.java
+++ src/main/java/onehundred_ohTen/task_903.java
@@ -1,4 +1,26 @@
 package onehundred_ohTen;
 
+import java.io.*;
+
 public class task_903 {
+    public static void main(String[] args) {
+        File input = new File("INPUT.TXT");
+        File output = new File("OUTPUT.TXT");
+        int res = 1;
+
+        try {
+            FileReader in = new FileReader(input);
+            FileWriter out = new FileWriter(output);
+            BufferedReader buffIn = new BufferedReader(in);
+            String number = buffIn.readLine();
+            res += Integer.valueOf(number);
+            out.write("" + res);
+            in.close();
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
